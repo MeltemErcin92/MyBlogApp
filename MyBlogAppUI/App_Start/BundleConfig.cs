592f3b3d280f6c965d7c8382c85b0d6b9b829467
user.MeltemErcin92 <mltmercin92@gmail.com>
2020-03-10 20:18:18 +0300

    first commit

---
+++ MyBlogAppUI/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MyBlogAppUI.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Content/LayoutScript").Include
+                (
+                "~/Content/style/js/bootstrap.min.js",
+               "~/Content/style/js/jquery.min.js",
+               "~/Content/style/js/plugins.js",
+               "~/Content/style/js/scripts.js")
+
+               );
+
+            bundles.Add(new StyleBundle("~/Content/LayoutCss")
+                .Include("~/Content/style/css/bootstrap.min.css",
+                "~/Content/style/css/plugins.css",
+                "~/Content/style.css",
+                 "~/Content/style/css/color/red.css",
+                  "~/Content/style/type/bebas.css",
+                  "~/Content/style/type/fontello.css"
+
+
+                )
+                );
+          
+        }
+       
+
+    }
+
+
+}
---
+++ MyBlogAppUI/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using MyBlog.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlogAppUI.Controllers
+{
+    public class HomeController :BaseController
+    {
+        BlogDbContext db = new BlogDbContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PopularArticles()
+        {
+            var popularArticles = db.Articles.OrderByDescending(x => x.CreateDate).Take(4).ToList();
+            return PartialView(popularArticles);
+        }
+    }
+}
